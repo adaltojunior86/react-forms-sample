b25e6b27a818f4e4be78eb786bba734f9722eb27
Adalto Junior <herdeiro.junior@gmail.com>
2018-11-25 14:48:09 -0300

    Visual adjusts.

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,30 +1,64 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
-import { MenuList, MenuItem } from '@material-ui/core';
 import './App.css';
+import {
+  AppBar, Tabs,
+  Tab,
+  Typography,
+  Toolbar,
+} from '@material-ui/core';
 import FormReduxForm from './forms/FormReduxForm';
-import formWithFormik from './forms/FormWithFormik';
+import FormWithFormik from './forms/FormWithFormik';
 import FormVanilla from './forms/FormVanilla';
 
-const App = () => (
-  <section>
-    <MenuList>
-      <MenuItem>
-        <Link to="/form-vanilla"> Form Vanilla </Link>
-      </MenuItem>
-      <MenuItem>
-        <Link to="/form-redux-form"> Form with redux-form </Link>
-      </MenuItem>
-      <MenuItem>
-        <Link to="/form-formik"> Form with Formik </Link>
-      </MenuItem>
-    </MenuList>
-    <Switch>
-      <Route path="/form-vanilla" component={FormVanilla} />
-      <Route path="/form-redux-form" component={FormReduxForm} />
-      <Route path="/form-formik" component={formWithFormik} />
-    </Switch>
-  </section>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tabSelected: 1,
+    };
+    this.onChangeTab = this.onChangeTab.bind(this);
+  }
+
+  onChangeTab = (event, value) => {
+    this.setState({
+      tabSelected: value,
+    });
+  }
+
+  renderForm = (tabSelected) => {
+    switch (tabSelected) {
+      case 1:
+        return <FormReduxForm />;
+      case 2:
+        return <FormWithFormik />;
+      default:
+        return <FormVanilla />;
+    }
+  }
+
+  render() {
+    const { tabSelected } = this.state;
+    return (
+      <section>
+        <AppBar position="relative">
+          <Toolbar>
+            <Typography variant="h3">
+              React Forms
+            </Typography>
+          </Toolbar>
+
+          <Tabs value={tabSelected} onChange={this.onChangeTab} centered>
+            <Tab label="Form Vanilla" />
+            <Tab label="Form with redux-form" />
+            <Tab label="Form with Formik" />
+          </Tabs>
+        </AppBar>
+        <section>
+          {this.renderForm(tabSelected)}
+        </section>
+      </section>
+    );
+  }
+}
 
 export default App;
