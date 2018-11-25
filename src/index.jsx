4e9fc5ec7a0da77ed4899bc3d314ff71fbd0a825
Adalto Junior <herdeiro.junior@gmail.com>
2018-11-25 13:46:10 -0300

    Removing react-router dom

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import { reducer as formReducer } from 'redux-form';
@@ -17,12 +16,7 @@ const store = (window.devToolsExtension
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-  , document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
