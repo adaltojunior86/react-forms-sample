6c9b0d64810805a3166231c2003d4c82b6c7385a
Adalto Junior <herdeiro.junior@gmail.com>
2021-09-17 22:32:20 -0300

    Adding cypress to project

--- cypress/integration/home_spec.js
+++ cypress/integration/home_spec.js
@@ -1,6 +1,6 @@
 describe('home actions', () => {
   it('page should be loaded', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit('http://localhost:5000');
     cy.get('header > div > h3').contains('React Forms')
   })
 })
